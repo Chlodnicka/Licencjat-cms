e722fb2157000ab5e259a7d4d72736902704e70d
Chlodnicka <chlodnicka.maja@gmail.com>
2016-06-13 16:19:14 +0200

    poprawki

--- app/lang/pl/validation.php
+++ app/lang/pl/validation.php
@@ -68,7 +68,7 @@ return array(
 		"string"  => "The :attribute must be :size characters.",
 		"array"   => "The :attribute must contain :size items.",
 	),
-	"unique"               => "The :attribute has already been taken.",
+	"unique"               => "Taki :attribute jest już w bazie danych.",
 	"url"                  => "The :attribute format is invalid.",
 
 	/*
