18919e533857f0a85620aa8f849948b13c84e139
Chlodnicka <chlodnicka.maja@gmail.com>
2016-06-15 14:16:20 +0200

    poprawka

---
+++ app/views/about.blade.php
@@ -0,0 +1,5 @@
+@section('content')
+ <h2>O serwisie</h2>
+    <p>Autorem niniejszego serwisu jest Maja Chłodnicka</p>
+    <p>Serwis ten stanowi integralną część pracy licencjackiej (kierunek: Elektroniczne Przetwarzanie Informacji), przygotowanej pod kierunkiem prof. dr. hab. Jerzego Koniora na Wydziale Zarządzania i Komunikacji Społecznej Uniwersytetu Jagiellońskiego.</p>
+@stop
